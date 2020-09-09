e2d7c443783255c18356bc0e1d84508a7303f800
DESKTOP-8K2UPF7\user <shoodol00@gmail.com>
2020-09-10 01:00:59 +0900

    for liveness test

---
+++ src/main/java/cnabookstore/Customer.java
@@ -0,0 +1,14 @@
+package cnabookstore;
+
+public class Customer {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
