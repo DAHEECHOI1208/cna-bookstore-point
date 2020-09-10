33865a5c39128236873a740e574fa88fe35cc4ba
DESKTOP-8K2UPF7\user <shoodol00@gmail.com>
2020-09-10 14:30:01 +0900

    liveness

---
+++ src/main/java/cnabookstore/ZombieController.java
@@ -0,0 +1,45 @@
+package cnabookstore;
+
+import cnabookstore.order.external.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ZombieController {
+
+    private boolean flag;
+
+    @Autowired
+    private CustomerService customerService;
+
+    public ZombieController(){
+        flag = true;
+    }
+
+    @GetMapping({"/isHealthy"})
+    public void zombie2() throws Exception {
+        if (flag)
+            return;
+        else
+            throw new Exception("zombie.....");
+    }
+
+    @GetMapping({"/makeZombie"})
+    public void getStockInputs() {
+
+        flag = false;
+
+    }
+
+    @GetMapping({"/orderService/verifyCustomer/{customerId}"})
+    public @ResponseBody String verifyCustomer(@PathVariable("customerId") Long customerId) {
+
+        customerService.queryCustomer(customerId);
+        return "Success...............";
+
+    }
+
+}
