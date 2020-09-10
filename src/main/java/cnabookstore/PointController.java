27ed7bd1f9f20f085a130142e55a4022d2b65178
DESKTOP-8K2UPF7\user <shoodol00@gmail.com>
2020-09-10 11:09:57 +0900

    circuitbreaker 추가

--- src/main/java/cnabookstore/PointController.java
+++ src/main/java/cnabookstore/PointController.java
@@ -1,11 +1,45 @@
 package cnabookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+import java.util.Optional;
+
 
 @RestController
  public class PointController {
 
+ @Autowired
+ PointRepository pointRepository;
 
+ @GetMapping("/selectPointInfo")
+ @HystrixCommand(fallbackMethod = "fallbackPoint", commandProperties = {
+         @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+         @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")
+ })
+ public String selectPointInfo(@RequestParam long pointId) throws InterruptedException {
+
+  if (pointId <= 0) {
+   System.out.println("@@@ CircuitBreaker!!!");
+   Thread.sleep(10000);
+   //throw new RuntimeException("CircuitBreaker!!!");
+  } else {
+   Optional<Point> point = pointRepository.findById(pointId);
+   return point.get().getPointValue().toString();
+  }
+
+  System.out.println("$$$ SUCCESS!!!");
+  return " SUCCESS!!!";
+ }
+
+ private String fallbackPoint(long pointId) {
+  System.out.println("### fallback!!!");
+  return "CircuitBreaker!!!";
+ }
 
 
  }
